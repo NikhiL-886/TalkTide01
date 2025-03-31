006461e68e8bff76b470305394b74ef1c783be1e
nikhil <nikhil.64360@gmail.com>
2025-03-31 12:49:48 +0530

    Deployment url added

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketProvider=({children})=>{
     useEffect(()=>{
 
         if(authUser){
-            const socket=io("http://localhost:4002",{
+            const socket=io("https://talktide-6g5n.onrender.com",{
                 query:{
                     userId:authUser.user._id
                 }
